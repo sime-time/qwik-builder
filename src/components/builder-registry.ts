1c6fad582da6bcb9e029d2054a4a3f73eb4f8322
Dunn, Simeon <simedunn01@gmail.com>
2024-07-14 17:33:45 -0400

    remove props route

--- src/components/builder-registry.ts
+++ src/components/builder-registry.ts
@@ -1,4 +1,30 @@
 import type { RegisteredComponent } from "@builder.io/sdk-qwik";
+import Props, { Item } from "../routes/props";
+import Joke from "../routes/joke";
+import Root from "../root";
+import Routes from "../routes";
 
+/**
+ * This array is used to integrate custom components within Builder.
+ * https://www.builder.io/c/docs/custom-components-intro
+ *
+ * These components will be found the "Custom Components"
+ * section of Builder's visual editor.
+ * You can also turn on "components only mode" to limit
+ * editing to only these components.
+ * https://www.builder.io/c/docs/guides/components-only-mode
+ */
 export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
+  {
+    component: Joke,
+    name: "Joke (Custom)",
+  },
+  {
+    component: Root,
+    name: "Root",
+  },
+  {
+    component: Routes,
+    name: "Routes",
+  },
 ];
